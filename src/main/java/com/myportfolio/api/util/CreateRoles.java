bd6a96dac5aedceff3e86ddbdcb762a55b416c68
Franco Brandan <brandanfranco0@gmail.com>
2022-07-31 20:16:29 +0200

    cambio

--- src/main/java/com/myportfolio/api/util/CreateRoles.java
+++ src/main/java/com/myportfolio/api/util/CreateRoles.java
@@ -39,7 +39,7 @@ public class CreateRoles implements
     public void onApplicationEvent(ContextRefreshedEvent event) {
       // Esta clase se ejecuta una sola vez para crear los roles /
     /**   Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
-        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);:
 
         rolService.save(rolAdmin);
         rolService.save(rolUser); //Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
